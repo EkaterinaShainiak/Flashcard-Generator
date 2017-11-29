8fbe6281dedf1ed68b991515bc5868889f43f6fa
Katrina Shainiak <ekaterina.shainiak@gmail.com>
2017-11-29 10:00:09 -0800

    added writing BasicCard in external file.

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
+// import external modules
+var fs = require("fs");
 var card = require("./card.js");
+var inquirer = require("inquirer");
 
+
+// creating a few cards
 var firsPresidentBasic = new card.BasicCard("Who was the first president of the United States?", "George Washington");
+addBasicQuestion(firsPresidentBasic);
 var firsPresidentCloze = new card.ClozeCard("... was the first president of the United States?", "George Washington");
+// addQuestion(firsPresidentCloze);
+// use npm inquirer package
+// inquirer.prompt(
+//     {
+//         type: 'confirm',
+//         name: "game",
+//         message: "THIS IS A CLOZE TEST. ARE YOU READY FOR QUESTIONS?",
+//         default: false
+//     }
+// ).then(function (err, answer) {
+//     if (err) {
+//         console.log(err);
+//         if (!game) {return}
+//         else {
+//             addQuestion(firsPresidentBasic);
 
-console.log(firsPresidentBasic);
-console.log(firsPresidentCloze);
+//         };
+//     };
+// });
+
+fs.appendFile("./cards.txt", "test", "utf8", function (err) {
+    if (err) throw err;
+
+});
+
+function addBasicQuestion(newQuestion) {
+    // questions.unshift(newQuestion);
+    var newQuestion = newQuestion.front + "," + newQuestion.back + "\n";
+    fs.appendFile("./cards.txt", newQuestion, "utf8", function (err) {
+        if (err) throw err;
+    })
+}
+
 
 
 
