8fee602fd11f9dc7833df1d89ba23ee05dc10412
Katrina Shainiak <ekaterina.shainiak@gmail.com>
2017-11-29 21:58:50 -0800

    added inquirer to ask user all questions and compare them with correct answers, added counter for lives

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cardBuilder = require("./card.js");
 var inquirer = require("inquirer");
 var cardStorageJson = require("./cards.json");
 var cardsStorageObj = [];
+var lives = 3;
 
 // creating a few cards
 
@@ -57,6 +58,10 @@ var cardsStorageObj = [];
 //         if (err) throw err;
 //     })
 // }
+
+
+
+
 fs.readFile("./cards.json", "utf8", function (err, res) {
     if (err) throw err;
     var resJson = JSON.parse(res);
@@ -68,31 +73,81 @@ fs.readFile("./cards.json", "utf8", function (err, res) {
         cardsStorageObj.push(card);
     };
     console.log(cardsStorageObj);
-
-
-
-    for (var i = 0; i < cardsStorageObj.length; i++) {
-        inquirer.prompt(
-            {
-                type: 'input',
-                name: 'user_answer',
-                message: cardsStorageObj[i].front,
-                default: false
-            }
-        ).then(function (err, answer) {
-            if (err) {
-                console.log(err);
-    
-            }
-            console.log("test" + answer);
-        });
-    
-    };
-
-
-
+    showCard();
 });
-console.log(cardsStorageObj.length);
+
+function showCard() {
+    console.log(cardsStorageObj.length);
+
+    // customise "when" for particular index/question
+    function toContinue(idx) {
+        return function (answers) {
+
+            if (!idx) return true;
+            console.log('  Checking:', answers['user_answer' + (idx - 1)], 'vs.', cardsStorageObj[idx - 1].back);
+            if (lives && (answers['user_answer' + (idx - 1)] != cardsStorageObj[idx - 1].back)) {
+                console.log('Wrong! Corect answer: ' + cardsStorageObj[idx - 1].back);
+                lives--;
+            }
+            return lives;
+        }
+    }
+    // create an array of questions as expected by inquirer.prompt()
+    var questions = [];
+    for (var i = 0; i < cardsStorageObj.length; i++) {
+        questions.push({
+            type: 'input',
+            name: 'user_answer' + i,
+            message: cardsStorageObj[i].front,
+            // using "when" to prematurely terminate flow (when returns false)
+            when: toContinue(i)
+        });
+    }
+    inquirer.prompt(questions)
+        .then(function (answers, err) {
+            if (err) throw err;
+            if (lives) {
+                toContinue(questions.length)(answers);
+            }
+            console.log('Lives:', lives);
+            console.log('You', lives && 'WON!' || 'LOST!')
+            console.log('Answers:', answers);
+        })
+        .catch(function (err) {
+            console.log("err", err);
+        })
+    // ---------------------------------------------------
+    // Alternative way - Replace loop to reqursion to prevent async flow:
+
+
+    // function ask(idx) {
+    //     inquirer.prompt(questions[idx]).then(answers => {
+    //         console.log(cardsStorageObj[idx].back + ' vs. ' + answers[idx])
+    //         if (idx != questions.length) {
+    //             ask(idx++);
+    //         }
+    //     });
+    // }
+
+    // ask(0)
 
 
 
+
+
+
+    // -----------------------------------------
+    // Attempt#1 -  All answers are coming together in one array
+
+
+    // var it = 0;
+    // inquirer.prompt(questions).then(function (answers, err) {
+    //     console.log('Question #' + ++it);
+    //     if (err) {
+    //         console.log("err", err);
+
+    //     }
+    // });
+    // -----------------------------------------
+
+};
