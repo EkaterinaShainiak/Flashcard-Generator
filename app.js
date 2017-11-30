441b48747a4c745d361ff7197bcd8065810f124e
Katrina Shainiak <ekaterina.shainiak@gmail.com>
2017-11-30 14:59:44 -0800

    removed unnecessary comments

--- app.js
+++ app.js
@@ -6,16 +6,6 @@ var cardStorageJson = require("./cards.json");
 var cardsStorageObj = [];
 var initLives = lives = 3;
 
-// creating a few cards
-
-// var firsPresidentBasic = new card.BasicCard("Who was the first president of the United States?", "George Washington");
-// addBasicQuestion(firsPresidentBasic);
-// var firsPresidentCloze = new card.ClozeCard("... was the first president of the United States?", "George Washington");
-// addClozeQuestion(firsPresidentCloze);
-
-// addQuestion(firsPresidentCloze);
-
-
 // use npm inquirer package
 inquirer.prompt(
     {
@@ -60,34 +50,6 @@ inquirer.prompt(
     }).catch(function (err) {
         console.log("err", err);
     })
-
-// Function to write new cards into json file
-
-// function addBasicQuestion(newQuestion) {
-//     // questions.unshift(newQuestion);
-//     // var newQuestion = newQuestion.front + "," + newQuestion.back + "\n";
-//     front = newQuestion.front;
-//     back = newQuestion.back;
-//     var newQuestionObj = {
-//         front: back
-//     };
-//     // var newQuestionJson = JSON.parse(newQuestion);
-//     fs.appendFile("./cards.json", newQuestion, "utf8", function (err) {
-//         if (err) throw err;
-//     })
-// }
-
-
-
-// function addClozeQuestion(newQuestion) {
-//     // questions.unshift(newQuestion);
-//     var newQuestion = newQuestion.partial + "," + newQuestion.cloze + "\n";
-//     fs.appendFile("./cards.json", newQuestion, "utf8", function (err) {
-//         if (err) throw err;
-//     })
-// }
-
-
 
 function readQuestions() {
     fs.readFile("./cards.json", "utf8", function (err, res) {
